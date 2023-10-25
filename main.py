dfbebc09c328639ecce9df73bc6d534b7ec14952
Vlad <vlad@MBP-Vlad.cn.ru_novotelecom.ru>
2023-10-25 12:06:29 +0700

    changes

--- main.py
+++ main.py
@@ -1,19 +1,10 @@
 from PIL import Image
 
-# Размер поля
 width, height = 1024, 1024
-
-# Создание пустого поля (белого)
 image = Image.new('1', (width, height), 1)
-
-# Начальная позиция муравья и начальное направление (вверх)
 x, y = 512, 512
 direction = 'up'
-
-# Число черных клеток
 black_cell_count = 0
-
-# Маппинг направлений
 directions = {
     'up': (0, -1),
     'right': (1, 0),
@@ -21,17 +12,15 @@ directions = {
     'left': (-1, 0)
 }
 
-# Функция для инвертирования пикселя
 def invert_pixel(x, y):
     pixel = image.getpixel((x, y))
     image.putpixel((x, y), 1 - pixel)
 
-# Движение муравья
-for _ in range(30000):  # Выполняем большое количество шагов (можно уменьшить)
-    # Получаем текущее значение клетки
+
+for _ in range(30000):
     current_pixel = image.getpixel((x, y))
 
-    if current_pixel == 1:  # Белая клетка
+    if current_pixel == 1:
         direction = {
             'up': 'right',
             'right': 'down',
@@ -41,7 +30,7 @@ for _ in range(30000):  # Выполняем большое количество
         invert_pixel(x, y)
         x += directions[direction][0]
         y += directions[direction][1]
-    else:  # Черная клетка
+    else:
         direction = {
             'up': 'left',
             'left': 'down',
@@ -53,7 +42,6 @@ for _ in range(30000):  # Выполняем большое количество
         y += directions[direction][1]
         black_cell_count += 1
 
-# Сохранение изображения
 image.save("ant_path.bmp")
 
 with open("black_cell_count.txt", "w", encoding="UTF-8") as f:
